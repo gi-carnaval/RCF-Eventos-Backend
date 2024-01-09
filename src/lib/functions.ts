413595992630a0b4f221c5ac9e03c114e22ccfcb
Giovani Carnaval <gi.carnaval91@gmail.com>
2024-01-08 22:47:38 -0300

    feat(installment): create function to get all Installments of a event

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,5 @@
+import { FullInstallmentInfo, InstallmentSummary } from "../types/installments";
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -67,3 +69,23 @@ export async function calcFinalValue(eventId: string | undefined) {
     throw new Error(`Erro ao calcular o valor total do evento: ${error}`);
   }
 }
+export async function getAllInstallments(eventId: string): Promise<FullInstallmentInfo[]>  {
+    const installments = await prisma.installments.findMany({
+      where: {
+        eventId
+      },
+      orderBy: {
+        installmentNumber: 'asc'
+      }
+    })
+    return installments
+}
+
+export async function getFirstInstallmentIdAndQuantityOfInstalls(installments: FullInstallmentInfo[]): Promise<InstallmentSummary> {
+  const firstInstallmentId = installments[0].id
+  const installmentValue = installments[0].installmentValue
+  const installmentsQuantity = installments.length
+
+  return {firstInstallmentId, installmentsQuantity, installmentValue}
+}
+
